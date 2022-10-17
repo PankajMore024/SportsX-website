87a1c613e10e87585217568d519de0d4413e59f9
PankajMore024 <84968982+PankajMore024@users.noreply.github.com>
2022-10-18 03:15:25 +0530

    Added initial files

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion: 6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  var fName = req.body.fName;
+  var lName = req.body.lName;
+  var email = req.body.email;
+
+
+
+  console.log(fName, lName, email);
+
+})
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
